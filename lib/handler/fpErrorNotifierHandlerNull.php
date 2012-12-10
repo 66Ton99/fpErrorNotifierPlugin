c6808bafdaeb555f048df010b16327e2b0f935d4
66Ton99 <66ton99@gmail.com>
2012-12-10 22:33:10 +0200

    Added NULL handler

---
+++ lib/handler/fpErrorNotifierHandlerNull.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author Ton Sharp <Forma-PRO@66ton99.org.ua>
+ */
+class fpErrorNotifierHandlerNull extends fpErrorNotifierHandler {
+
+  /**
+   * @param Exception $e
+   */
+  public function handleException(Exception $e)
+  {
+  }
+
+  /**
+   *
+   */
+  public function initialize()
+  {
+
+  }
+}
